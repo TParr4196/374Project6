f79dfb34932375e46c5baf95f85aab1de56dcbd3
TParr4196 <tparr@LAPTOP-0O0FGHKA>
2024-02-18 20:56:35 -0800

    one off initialization

--- myalloc.c
+++ myalloc.c
@@ -8,6 +8,7 @@
 #define PADDED_SIZE(x) ((x) + GET_PAD(x))
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
 #define padded_struct_block_size = PADDED_SIZE(sizeof(struct block));
+struct block *head = NULL;  // Head of the list, empty
 
 struct block {
     int size;            // Size in bytes
@@ -16,15 +17,18 @@ struct block {
 };
 
 void *myalloc(int size){
-    struct block *head = NULL;  // Head of the list, empty
-    void *heap = mmap(NULL, 1024, PROT_READ|PROT_WRITE,
-                  MAP_ANON|MAP_PRIVATE, -1, 0);
-    return heap; //for warnings
+    if(head==NULL){
+        void *heap = mmap(NULL, 1024, PROT_READ|PROT_WRITE,
+                MAP_ANON|MAP_PRIVATE, -1, 0);
+        head=heap;
+        int data_size = 1024 - PADDED_SIZE(sizeof(struct block));
+        head->size = data_size;
+        head->in_use = 0;
+    }
+    return head;
 }
 
-void myfree(void *p){
-
-}
+//void myfree(void *p);
 
 int main(){
 
