663df67cc4670defaba816c85033b301d59f9d9d
Tyler <tplaxstar@gmail.com>
2024-02-18 21:10:54 -0800

    myalloc searches list and marks as in use

--- myalloc.c
+++ myalloc.c
@@ -25,11 +25,49 @@ void *myalloc(int size){
         head->size = data_size;
         head->in_use = 0;
     }
-    return head;
+    struct block *temp=head;
+    while(temp!=NULL){
+        if(temp->in_use==0){
+            if(temp->size>=size){
+                temp->in_use=1;
+                return PTR_OFFSET(temp,PADDED_SIZE(sizeof(struct block)));
+            }
+        }
+        temp=temp->next;
+    }
+    return NULL;
 }
 
 //void myfree(void *p);
 
-int main(){
+void print_data(void)
+{
+    struct block *b = head;
 
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
+
+int main(){
+    void *p;
+    print_data();
+    p = myalloc(16);
+    print_data();
+    p = myalloc(16);
+    printf("%p\n", p);
 }
